566a2c5b569661eca2188945d84127390b1e9a05
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-22 19:17:02 +0100

    chore: refactor migrations and add accounts table
    This moves the migration file from inner nesting to database/migrations

---
+++ src/database/migrations/20240622180459_account.ts
@@ -0,0 +1,17 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('account', table => {
+        table.increments('id')
+        table.decimal('balance', 2)
+        table.bigInteger('user_id').references('id').inTable('users').unique()
+        table.timestamps(true, true)
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('account')
+}
+
