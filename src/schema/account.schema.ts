27d4147e9c592c16b3341ed7b1ef10e362b6ac16
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-23 19:51:12 +0100

    chore: add schemas to validate request bodies
    Add schemas to validate request bodies when sending and withdrawing
    funds from an account.

--- src/schema/account.schema.ts
+++ src/schema/account.schema.ts
@@ -7,4 +7,19 @@ export const fundAccountSchema = object({
     })
 })
 
+export const transferFundsSchema = object({
+    body: object({
+        recipientUserId: number({ required_error: 'recipientUserId is required' }),
+        amount: number({ required_error: 'amount to be transferred is required' }).positive('amount to be transferred cannot be negative')
+    })
+})
+
+export const withdrawFundsSchema = object({
+    body: object({
+        amount: number({ required_error: 'amount to be withdrawn is required' }).positive('amount to be withdrawn cannot be negative')
+    })
+})
+
 export type FundAccountInput = TypeOf<typeof fundAccountSchema>
+export type TransferFundsInput = TypeOf<typeof transferFundsSchema>
+export type WithdrawFundsInput = TypeOf<typeof withdrawFundsSchema>
