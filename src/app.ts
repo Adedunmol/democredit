40e16a131bef73fe6353a73174c8423308708768
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-23 20:10:10 +0100

    feat: add route to withdraw and transfer funds

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import userRouter from './routes/user.route'
 import accountRouter from './routes/account.route'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/v1/auth', userRouter)
+
+app.use(verifyJWT)
 app.use('api/v1/accounts', accountRouter)
 
 export default app
--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import validateResource from '../middlewares/validate-resource';
-import { fundAccountSchema } from '../schema/account.schema';
-import { fundAccountController } from '../controller/account.controller';
+import { fundAccountSchema, transferFundsSchema, withdrawFundsSchema } from '../schema/account.schema';
+import { fundAccountController, transferFundsController, withdrawFundsController } from '../controller/account.controller';
 
 const router = Router()
 
 router.route('/fund').post(validateResource(fundAccountSchema), fundAccountController)
+router.route('/transfer').post(validateResource(transferFundsSchema), transferFundsController)
+router.route('/withdraw').post(validateResource(withdrawFundsSchema), withdrawFundsController)
 
 export default router
