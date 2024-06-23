6bfedf88acde5f858718ade9c5cd5010fb42c78c
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-23 20:01:37 +0100

    feat: add controllers to withdraw and transfer funds

--- src/controller/account.controller.ts
+++ src/controller/account.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { FundAccountInput } from "../schema/account.schema";
-import { fundAccountService } from "../service/account.service";
+import { FundAccountInput, TransferFundsInput, WithdrawFundsInput } from "../schema/account.schema";
+import { fundAccountService, transferFundsService, withdrawFundsService } from "../service/account.service";
 
 
 export const fundAccountController = async (req: Request<{}, {}, FundAccountInput['body']>, res: Response) => {
@@ -12,5 +12,31 @@ export const fundAccountController = async (req: Request<{}, {}, FundAccountInpu
         console.log(err)
 
         return res.status(500).json({ status: 'error', message: 'error funding account', data: null })
+    }
+}
+
+export const transferFundsController = async (req: Request<{}, {}, TransferFundsInput['body']>, res: Response) => {
+    try {
+        // @ts-ignore
+        const account = await transferFundsService({ ...req.body, senderUserId: req.user.id })
+
+        return res.status(200).json({ status: 'success', message: 'transaction successful', data: { account } })
+    } catch (err: any) {
+        console.log(err)
+
+        return res.status(400).json({ status: 'error', message: err.message, data: null })
+    }
+}
+
+export const withdrawFundsController = async (req: Request<{}, {}, WithdrawFundsInput['body']>, res: Response) => {
+    try {
+        // @ts-ignore
+        const account = await withdrawFundsService({ ...req.body, userId: req.user.id })
+
+        return res.status(200).json({ status: 'success', message: 'withdraw successful', data: { account } })
+    } catch (err: any) {
+        console.log(err)
+
+        return res.status(400).json({ status: 'error', message: err.message, data: null })
     }
 }
