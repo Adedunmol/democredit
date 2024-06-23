576583e7381721b9c041eb4ad22c8eaaede941ed
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-23 12:32:27 +0100

    chore: add Karma class
    Add karma class to check if user is on karma blacklist.

---
+++ src/adjutor/index.ts
@@ -0,0 +1,24 @@
+
+class Karma {
+    private apiKey: string
+    private baseUrl: string
+
+    constructor(apiKey: string) {
+        this.apiKey = apiKey
+        this.baseUrl = 'https://adjutor.lendsqr.com/v2'
+    }
+
+    async isOnBlacklist(email: string) {
+        const url = `${this.baseUrl}/verification/karma/${email}`
+        const resp = await fetch(url, { headers: { 'Authorization': `Bearer ${this.apiKey}` } })
+
+        const data = await resp.json()
+        const user = data.data
+
+        if (!user) return false
+
+        return true
+    }
+}
+
+export default new Karma(process.env.ADJUTOR_API_KEY!!)
