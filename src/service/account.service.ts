bf230018452c6219af1f96e80bf1b05a574411c8
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-24 22:05:03 +0100

    refactor: move functions into account service class

--- src/service/account.service.ts
+++ src/service/account.service.ts
@@ -1,33 +1,41 @@
 import accountModel from "../models/account.model"
 
-export const createAccountService = async (data: any) => {
-    const { userId: user_id } = data
+class AccountService {
 
-    return accountModel.createAccount({ user_id })
+    createAccount(data: any) {
+        const { userId: user_id } = data
+    
+        return accountModel.createAccount({ user_id })
+    }
+
+    fundAccount(data: any) {
+        const { userId: user_id, amount } = data
+    
+        return accountModel.fundAccount({ user_id, amount })
+    }
+
+    async transferFunds(data: any) {
+        const { senderUserId, recipientUserId, amount } = data
+    
+        const account = await accountModel.getAccount(senderUserId)
+    
+        // custom error insufficient balance
+        if (account && amount > account.balance) throw new Error('Insuffucient balance')
+    
+        return accountModel.transferFunds({ senderUserId, recipientUserId, amount })
+    }
+
+    async withdrawFunds(data: any) {
+        const { userId: user_id, amount } = data
+    
+        const account = await accountModel.getAccount(user_id)
+    
+        if (account && amount > account.balance) throw new Error('Insuffucient balance')
+    
+        return accountModel.withdrawFunds({ user_id, amount })
+    }
 }
 
-export const fundAccountService = async (data: any) => {
-    const { userId: user_id, amount } = data
+// include validation during transfer
 
-    return accountModel.fundAccount({ user_id, amount })
-}
-
-export const transferFundsService = async (data: any) => {
-    const { senderUserId, recipientUserId, amount } = data
-
-    const account = await accountModel.getAccount(senderUserId)
-
-    if (account && amount > account.balance) throw new Error('Insuffucient balance')
-
-    return accountModel.transferFunds({ senderUserId, recipientUserId, amount })
-}
-
-export const withdrawFundsService = async (data: any) => {
-    const { userId: user_id, amount } = data
-
-    const account = await accountModel.getAccount(user_id)
-
-    if (account && amount > account.balance) throw new Error('Insuffucient balance')
-
-    return accountModel.withdrawFunds({ user_id, amount })
-}
+export default new AccountService()
