c05f89b4ed73bab7db21dda58ab8486ce3c545b7
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-25 19:48:03 +0100

    fix: remove console.log of env variables

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,7 +3,6 @@ import config from './knexfile'
 
 const configEnv = process.env.ENVIRONMENT === 'development' ? config.development : config.production
 
-console.log(configEnv)
 const db = knex(configEnv)
 
 export default db
