6e457d60ab7fe063857f5789af52598214e6c639
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-25 19:22:23 +0100

    chore: change config for knex during deployment

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -12,6 +12,21 @@ const config: { [key: string]: Knex.Config } = {
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'mysql2',
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       host: process.env.DATABASE_HOST,
       port: Number(process.env.DATABASE_PORT!!)
     },
